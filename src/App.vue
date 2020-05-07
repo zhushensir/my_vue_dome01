06db2344ae65bb2729665e63f4b77a56a87d5ccf
zhushen <137895356@qq.com>
2020-05-07 09:18:05 +0800

    简单的抄写，关于组件的理解关于vue脚手架的理解。

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <header class="site-header jumbotron">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12">
+            <h1>请发表对React的评论</h1>
+          </div>
+        </div>
+      </div>
+    </header>
+    <div class="container">
+    <Add/>
+    <List/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Add from "./components/Add";
+import List from "./components/List";
+export default {
+  components:{
+    Add,
+    List
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p class="msg">{{msg}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "helloVue"
+    };
+  }
+};
+</script>
+<style>
+.msg {
+  color: burlywood;
+  font-size: 30px;
+}
+</style>
